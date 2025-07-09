90813a8837c90d18ba689ad9695d2231601084a9
KHAUSMC <diopk5177@gmail.com>
2025-07-08 21:25:05 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+int main()
+{
+    char op;
+    double num1;
+    double num2;
+    double result;
+
+    std::cout << "************ CALCULATOR ************\n";
+
+    std::cout << "Ener either (+ - * /): ";
+    std::cin >> op;
+
+    std::cout << "Enter #1; ";
+    std::cin >> num1;
+
+    std::cout << "Enter #2 ";
+    std::cin >> num2;
+
+    switch (op)
+    {
+    case '+':
+        result = num1 + num2;
+        std::cout << "result " << result << "\n";
+        break;
+    case '-':
+        result = num1 - num2;
+        std::cout << "result " << result << "\n";
+        break;
+    case '*':
+        result = num1 * num2;
+        std::cout << "result " << result << "\n";
+        break;
+    case '/':
+        result = num1 / num2;
+        std::cout << "result " << result << "\n";
+        break;
+    }
+
+    std::cout << "**************************************";
+
+    return 0;
+}
